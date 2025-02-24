41c84810f9b7cec7c1111c456299fe04f92a6bf4
cavalenciad <cavalenciad@outlook.com>
2025-02-24 15:14:43 -0500

    starting the backend - Add models and contracts for user

---
+++ Contracts/CreateUserRequest.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Contracts
+{
+    public class CreateUserRequest
+    {
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "El nombre debe tener entre 3 y 20 caracteres")]
+        public required string Name { get; set; }
+        
+        [Required(ErrorMessage = "El email es requerido")]
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "El email debe tener al menos 8 caracteres")]
+        public required string Email { get; set; }
+        
+        [Range(18, 80, ErrorMessage = "La edad debe estar entre 18 y 80 años")]
+        public required int Age { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        public int Age { get; set; }
+
+        public User(){}
+    }
+}
